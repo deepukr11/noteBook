617889f0e2d5b664cd7a6a9a0f7b32ba79abc6e2
deepukr11 <deepukumar05455@gmail.com>
2023-08-02 20:44:35 +0530

    3nd

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Notes = require('../models/notes');
+const Notes = require('../models/Notes');
 const fetchuser = require('../midleware/fetchuser');
 const { body, validationResult } = require('express-validator');
 
